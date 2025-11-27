3b1a495f1fbd53092036689f9fdb5b4d6cf81ecc
ExVirabot <18904015391@163.com>
2025-11-27 10:49:29 +0800

    Add files via upload

--- clientgui.py
+++ clientgui.py
@@ -3,22 +3,22 @@ import threading
 import queue
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QTextEdit, QLineEdit
-from PySide6.QtCore import QTimer, Slot, QRect
+from PySide6.QtCore import Qt, QTimer, Slot, QRect
 from PySide6.QtGui import QImage, QPixmap, QPainter
 import pygame
+
 HOST = 'frp-oil.com'
 PORT = 63867
-started = threading.Event()
 closed = threading.Event()
 readqueue = queue.Queue()
-quandict = {"剪刀":"0","石头":"1","布":"2"}
+sendlock = threading.Lock()
+quandict = {"剪刀": "0", "石头": "1", "布": "2"}
+
 
 def reader(s):
     with s.makefile('r', encoding='utf-8') as f:
         while True:
             msg = f.readline().strip()
-            if msg == "游戏开始！":
-                started.set()
             if not msg:
                 break
             readqueue.put(msg)
@@ -27,58 +27,91 @@ def reader(s):
     closed.set()
 
 
-class PygButton:
-    def __init__(self, topleft, size, text='', bg=(200,200,200), down=(140,140,140), text_color=(0,0,0)):
+class PygameButton:
+    def __init__(self, topleft=(0, 0), text=("", ""), buttontype=0, bg=(200, 200, 200), hover=(170, 170, 170),
+                 down=(140, 140, 140), text_color=(0, 0, 0)):
+        size = (155, 50)
+        if buttontype:
+            PAx = 10
+            PAy = 520
+            if buttontype == 1:
+                topleft = (10 - PAx, 555 - PAy)
+                size = (20, 40)
+            elif buttontype == 2:
+                topleft = (730 - PAx, 555 - PAy)
+                size = (20, 40)
+            elif buttontype == 3:
+                topleft = (780 - PAx, 590 - PAy)
+                size = (50, 30)
+                text = ("全部", "")
+        self.buttontype = buttontype
         self.rect = pygame.Rect(topleft, size)
-        self.text = text
+        self.display = text[0]
+        self.send = text[1]
         self.bg = bg
+        self.hover = hover
         self.down = down
         self.text_color = text_color
         self._down = False
+        self._hover = False
 
     def draw(self, surf, font):
         color = self.bg
         if self._down:
             color = self.down
-        pygame.draw.rect(surf, color, self.rect, border_radius=6)
-        pygame.draw.rect(surf, (0,0,0), self.rect, 2, border_radius=6)
-        if self.text:
-            txt_surf = font.render(self.text, True, self.text_color)
+        elif self._hover:
+            color = self.hover
+        if self.buttontype:
+            if self.buttontype == 1:
+                pygame.draw.polygon(surf, color, [self.rect.midleft, self.rect.topright, self.rect.bottomright])
+                pygame.draw.polygon(surf, (0, 0, 0), [self.rect.midleft, self.rect.topright, self.rect.bottomright], 2)
+            elif self.buttontype == 2:
+                pygame.draw.polygon(surf, color, [self.rect.midright, self.rect.topleft, self.rect.bottomleft])
+                pygame.draw.polygon(surf, (0, 0, 0), [self.rect.midright, self.rect.topleft, self.rect.bottomleft], 2)
+            elif self.buttontype == 3:
+                pygame.draw.rect(surf, color, self.rect, border_radius=6)
+                pygame.draw.rect(surf, (0, 0, 0), self.rect, 2, border_radius=6)
+        else:
+            pygame.draw.rect(surf, color, self.rect, border_radius=6)
+            pygame.draw.rect(surf, (0, 0, 0), self.rect, 2, border_radius=6)
+        if self.display:
+            txt_surf = font.render(self.display, True, self.text_color)
             tr = txt_surf.get_rect(center=self.rect.center)
             surf.blit(txt_surf, tr)
 
-    def handle_mouse_down(self, pos, button=1):
-        if started.is_set() and button == 1 and self.rect.collidepoint(pos):
+    def handle_mouse_down(self, pos):
+        if self.rect.collidepoint(pos):
             self._down = True
-            return True
-        return False
+        return
 
-    def handle_mouse_up(self, pos, button=1):
-        clicked = False
-        if button == 1:
-            if self._down and self.rect.collidepoint(pos):
-                clicked = True
-            self._down = False
+    def handle_mouse_up(self, pos):
+        clicked = self._down and self.rect.collidepoint(pos)
+        self._down = False
         return clicked
 
+    def handle_mouse_hover(self, pos):
+        self._hover = self.rect.collidepoint(pos)
+
+
 class PygameArea(QWidget):
-    def __init__(self, parent=None, geom=QRect(50, 580, 651, 50)):
+    def __init__(self, parent=None, geom=QRect(10, 520, 820, 110)):
         super().__init__(parent)
+        self.setMouseTracking(True)
         self.setGeometry(geom)
         self.w = geom.width()
         self.h = geom.height()
 
         pygame.font.init()
         self.font = pygame.font.SysFont("SimHei", 24)
-
         self.surface = pygame.Surface((self.w, self.h))
 
-        btn_size = (197, 50)
-        global_positions = [(50, 580), (277, 580), (504, 580)]
+        global_positions = [(40, 520), (215, 520), (390, 520), (565, 520), (40, 580), (215, 580), (390, 580),
+                            (565, 580)]
         topleft = (geom.x(), geom.y())
-        rel_positions = [(x - topleft[0], y - topleft[1]) for (x,y) in global_positions]
-        labels = ["剪刀", "石头", "布"]
-        self.buttons = [PygButton(pos, btn_size, text=lab) for pos, lab in zip(rel_positions, labels)]
+        self.rel_positions = [(x - topleft[0], y - topleft[1]) for (x, y) in global_positions]
+        self.texts = []
+        self.chatmode = "q"
+        self.update_button(1)
 
         self._qpix = None
 
@@ -88,13 +121,30 @@ class PygameArea(QWidget):
 
         self.on_button_clicked = None
 
+    def update_button(self, page):
+        self.buttons = [PygameButton(buttontype=3)]
+        if self.chatmode == "d":
+            self.buttons[0].display = "队伍"
+        self.page = page
+        for pos, text in zip(self.rel_positions, self.texts[(page - 1) * 8:]):
+            self.buttons.append(PygameButton(pos, text))
+        buttonnum = len(self.texts)
+        self.pagemax = (buttonnum + 6) // 8
+        if self.page > 1:
+            self.buttons.append(PygameButton(buttontype=1))
+        if self.pagemax > page:
+            self.buttons.append(PygameButton(buttontype=2))
+
     def on_timeout(self):
-        self.render_frame()
-        self.update()
+        try:
+            self.render_frame()
+            self.update()
+        except KeyboardInterrupt:
+            closed.set()
 
     def render_frame(self):
-        self.surface.fill((240,240,240))
-
+        self.surface.fill((240, 240, 240))
+        # print(len(self.buttons))
         for b in self.buttons:
             b.draw(self.surface, self.font)
 
@@ -110,18 +160,47 @@ class PygameArea(QWidget):
     def _map_pos(self, qpos):
         return (int(qpos.x()), int(qpos.y()))
 
-
     def mousePressEvent(self, ev):
-        pos = self._map_pos(ev.position().toPoint())
-        for b in self.buttons:
-            b.handle_mouse_down(pos, button=1)
+        if ev.button() == Qt.LeftButton:
+            pos = self._map_pos(ev.position().toPoint())
+            for b in self.buttons:
+                b.handle_mouse_down(pos)
 
     def mouseReleaseEvent(self, ev):
+        if ev.button() == Qt.LeftButton:
+            pos = self._map_pos(ev.position().toPoint())
+            for b in self.buttons:
+                if b.handle_mouse_up(pos):
+                    if b.buttontype:
+                        if b.buttontype == 1:
+                            self.update_button(self.page - 1)
+                        elif b.buttontype == 2:
+                            self.update_button(self.page + 1)
+                        elif b.buttontype == 3:
+                            if self.chatmode == "q":
+                                self.chatmode = "d"
+                                self.buttons[0].display = "队伍"
+                            else:
+                                self.chatmode = "q"
+                                self.buttons[0].display = "全部"
+
+                    else:
+                        self.texts = []
+                        self.update_button(1)
+                        self.page = 1
+                        with sendlock:
+                            s.sendall((b.send + "\n").encode('utf-8'))
+                    break
+
+    def mouseMoveEvent(self, ev):
         pos = self._map_pos(ev.position().toPoint())
         for b in self.buttons:
-            if b.handle_mouse_up(pos, button=1):
-                if callable(self.on_button_clicked):
-                    self.on_button_clicked(b.text)
+            b.handle_mouse_hover(pos)
+
+    def leaveEvent(self, ev):
+        for b in self.buttons:
+            b._hover = False
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -131,9 +210,12 @@ class MainWindow(QWidget):
         self.setObjectName("mainWindow")
         self.setStyleSheet("#mainWindow { background-color: rgb(240,240,240); }")
 
+        button_region = QRect(10, 520, 820, 110)
+        self.pyg_area = PygameArea(self, geom=button_region)
+
         self.left_output = QTextEdit(self)
         self.left_output.setReadOnly(True)
-        self.left_output.setGeometry(20, 20, 711, 550)
+        self.left_output.setGeometry(20, 20, 720, 490)
 
         self.right_output = QTextEdit(self)
         self.right_output.setReadOnly(True)
@@ -141,12 +223,8 @@ class MainWindow(QWidget):
 
         self.input_line = QLineEdit(self)
         self.input_line.setPlaceholderText("输入聊天信息…")
-        self.input_line.setGeometry(780, 590, 320, 30)
+        self.input_line.setGeometry(830, 590, 270, 30)
         self.input_line.returnPressed.connect(self.on_input_entered)
-
-        btn_region = QRect(50, 580, 651, 50)
-        self.pyg_area = PygameArea(self, geom=btn_region)
-        self.pyg_area.on_button_clicked = self.on_pyg_button_clicked
 
         self._close_timer = QTimer(self)
         self._close_timer.timeout.connect(self._check_closed_event)
@@ -156,22 +234,34 @@ class MainWindow(QWidget):
     def on_input_entered(self):
         msg = self.input_line.text().strip()
         if msg:
-            s.sendall(("/q " + msg + "\n").encode('utf-8'))
+            with sendlock:
+                if self.pyg_area.chatmode == "d":
+                    s.sendall(("/d " + msg + "\n").encode('utf-8'))
+                else:
+                    s.sendall(("/q " + msg + "\n").encode('utf-8'))
             self.input_line.clear()
 
-    def on_pyg_button_clicked(self, label):
-        msg = quandict[label]
-        s.sendall((msg + "\n").encode('utf-8'))
-
-    def _check_closed_event(self):
+    def _check_closed_event(self):  # 兼有接受信息功能
         try:
-            msg = readqueue.get(block=False)
-            if msg.startswith("/q") or msg.startswith("/d"):
-                self.right_output.append(msg[2:].strip())
-            else:
-                self.left_output.append(msg)
+            while True:
+                msg = readqueue.get(block=False)
+                if msg.startswith("/q") or msg.startswith("/d"):
+                    self.right_output.append(msg[2:].strip())
+                elif msg.startswith("/b"):
+                    self.pyg_area.texts = []
+                    msg = msg[2:].strip()
+                    while msg:
+                        display, send, msg = msg.split(";", 2)
+                        self.pyg_area.texts.append((display, send))
+                    # print(len(self.pyg_area.texts))
+                    self.pyg_area.update_button(1)
+
+                else:
+                    self.left_output.append(msg)
         except queue.Empty:
             pass
+        except KeyboardInterrupt:
+            closed.set()
 
         if closed.is_set():
             self.close()
@@ -179,15 +269,20 @@ class MainWindow(QWidget):
 
 if __name__ == '__main__':
     name = str(input("请输入昵称：")).strip()
+    team = str(input("请输入队伍名：")).strip()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
-    s.sendall((name + "\n").encode('utf-8'))
-    t = threading.Thread(target=reader, args=(s,), name="reader")
-    t.start()
+    try:
+        s.connect((HOST, PORT))
+        with sendlock:
+            s.sendall((name + "\n" + team + "\n").encode('utf-8'))
+        t = threading.Thread(target=reader, args=(s,), name="reader")
+        t.start()
 
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
-    app.exec()
-    closed.set()
-    s.shutdown(socket.SHUT_RDWR)
+        app = QApplication(sys.argv)
+        win = MainWindow()
+        win.show()
+        app.exec()
+    finally:
+        closed.set()
+        s.shutdown(socket.SHUT_RDWR)
+        s.close()
